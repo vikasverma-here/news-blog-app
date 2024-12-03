f2bc34a28f7e59b09095ecaef4b2fc244b238c4c
vikasverma-here <vikasverma995619@gmail.com>
2024-12-03 17:23:05 +0530

    50% layout done

--- news-blogs-app/src/component/News.jsx
+++ news-blogs-app/src/component/News.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
-
+import Weather from './weather'
+import Calander from './calander'
+import './news.css'
 const News = () => {
   return (
-    <div>
-      <h1>news</h1>
+   <div className="news">
+    <header className='news-header'>News Header</header>
+    <div className="news-content">
+      <div className="navbar">
+        <div className="user">User</div>
+        <nav className="catagories">Categories</nav>
+      </div>
+      <div className="news-section">
+        <div className="headline">Headline</div>
+        <div className="news-grid">News Grid</div>
+      </div>
+      <div className="my-blogs">My-blogs</div>
+      <div className="weather-calender">
+      <Weather/>
+      <Calander/>
+      </div>
+    
+
     </div>
+    <footer className="news-footer">
+        Footer
+      </footer>
+   </div>
   )
 }
 
---
+++ news-blogs-app/src/component/calander.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Calander = () => {
+  return (
+    <div>
+      <h1>Calender</h1>
+    </div>
+  )
+}
+
+export default Calander
