47b4a370fc49b140eadfa068730cc3fc6d504425
vikasverma-here <vikasverma995619@gmail.com>
2024-12-08 16:53:13 +0530

    completed two section layout and styling

--- news-blogs-app/src/component/News.jsx
+++ news-blogs-app/src/component/News.jsx
@@ -2,14 +2,44 @@ import React from 'react'
 import Weather from './weather'
 import Calander from './calander'
 import './news.css'
+import userimage from '../assets/images/user.jpg'
 const News = () => {
   return (
    <div className="news">
-    <header className='news-header'>News Header</header>
+    <header className='news-header'>
+      <h1 className="logo">News-blogs</h1>
+      <div className="search-bar">
+      <form action="">
+        <input type="text" placeholder='Search News....' />
+        <button type="submit" > <i className='fa-solid fa-magnifying-glass'  > </i></button>
+      </form>
+      </div>
+      
+    </header>
     <div className="news-content">
       <div className="navbar">
-        <div className="user">User</div>
-        <nav className="catagories">Categories</nav>
+        <div className="user">
+          <img src={userimage} alt="" />
+          <p>Vikas's Blog</p>
+        </div>
+        <nav className="catagories">
+          <h1 className="nav-heading">Categories</h1>
+          <div className="nav-links">
+            <a href="#" className='nav-link' >Genral</a>
+            <a href="#" className='nav-link' >World</a>
+            <a href="#" className='nav-link' >Business</a>
+            <a href="#" className='nav-link' >Technology</a>
+            <a href="#" className='nav-link' >Entertainment</a>
+            <a href="#" className='nav-link' >Sports</a>
+            <a href="#" className='nav-link' >Science</a>
+            <a href="#" className='nav-link' >Health</a>
+            <a href="#" className='nav-link' >Nation</a>
+            <a href="#" className='nav-link' >
+              Bookmarks
+              <i className="fa-regular fa-bookmark"></i>
+            </a>
+          </div>
+        </nav>
       </div>
       <div className="news-section">
         <div className="headline">Headline</div>
